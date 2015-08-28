68be5eaf7a7950356459da00afadf5ff128c98a8
sunxueye <317026025@qq.com>
2015-08-28 18:04:50 +0800

    value object

---
+++ sluck-core/src/main/java/com/sluckframework/domain/valueobject/ValueObject.java
@@ -0,0 +1,14 @@
+package com.sluckframework.domain.valueobject;
+
+import java.io.Serializable;
+
+/**
+ * 值对象，不可变，用于度量或描述领域内的概念，需要改变直接替换
+ * 
+ * @author sunxy
+ * @time 2015年8月28日 上午11:32:29	
+ * @since 1.0
+ */
+public interface ValueObject extends Serializable {
+
+}
