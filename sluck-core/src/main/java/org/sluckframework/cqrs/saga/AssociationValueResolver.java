aae1d00b75cb15a7d5776e25b992fbc0429403b7
sunxueye <317026025@qq.com>
2015-09-12 23:15:06 +0800

    as

---
+++ sluck-core/src/main/java/org/sluckframework/cqrs/saga/AssociationValueResolver.java
@@ -0,0 +1,25 @@
+package org.sluckframework.cqrs.saga;
+
+import org.sluckframework.domain.event.EventProxy;
+
+import java.util.Set;
+
+/**
+ * 关联值解析器,用于解析关联值
+ *
+ * Author: sunxy
+ * Created: 2015-09-12 23:13
+ * Since: 1.0
+ */
+public interface AssociationValueResolver {
+
+    /**
+     *
+     * 从给定的事件提取 关联值
+     * Extracts an Association Value from the given <code>event</code>.
+     *
+     * @param event The event to extract Association Value from
+     * @return The Association Value extracted from the Event, or <code>null</code> if none found.
+     */
+    Set<AssociationValue> extractAssociationValues(EventProxy<?> event);
+}
