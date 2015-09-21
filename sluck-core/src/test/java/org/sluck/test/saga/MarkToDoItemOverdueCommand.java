32bdc64cb934f066df018e60b3bf465bfba6803a
sunxueye <317026025@qq.com>
2015-09-21 22:55:58 +0800

    identifier test

--- sluck-core/src/test/java/org/sluck/test/saga/MarkToDoItemOverdueCommand.java
+++ sluck-core/src/test/java/org/sluck/test/saga/MarkToDoItemOverdueCommand.java
@@ -2,18 +2,19 @@
 package org.sluck.test.saga;
 
 import org.sluckframework.cqrs.commandhandling.annotation.TargetAggregateIdentifier;
+import org.sluckframework.domain.identifier.DefaultIdentifier;
 
 
 public class MarkToDoItemOverdueCommand  {
 
     @TargetAggregateIdentifier
-    private final String todoId;
+    private final DefaultIdentifier todoId;
 
-    public MarkToDoItemOverdueCommand(String todoId) {
+    public MarkToDoItemOverdueCommand(DefaultIdentifier todoId) {
         this.todoId = todoId;
     }
 
-    public String getTodoId() {
+    public DefaultIdentifier getTodoId() {
         return todoId;
     }
 }
