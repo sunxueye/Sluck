573c6ba7575cd8478264ddc89c9a3d0ac467d7c1
sunxueye <317026025@qq.com>
2015-10-29 18:06:17 +0800

    update command generateor

--- sluck-quickstart/src/test/java/org/sluckframework/demo/test/member/CommandGenerator.java
+++ sluck-quickstart/src/test/java/org/sluckframework/demo/test/member/CommandGenerator.java
@@ -3,7 +3,8 @@ package org.sluckframework.demo.test.member;
 
 import org.sluckframework.cqrs.commandhandling.gateway.CommandGateway;
 import org.sluckframework.demo.member.MemberID;
-import org.sluckframework.demo.member.command.UpdateMemberInfoCommand;
+import org.sluckframework.demo.member.MerchantID;
+import org.sluckframework.demo.member.command.CreateMerchantCommand;
 
 
 public class CommandGenerator {
@@ -11,10 +12,12 @@ public class CommandGenerator {
     public static void sendCommands(CommandGateway commandGateway) {
 //        final DefaultIdentifier itemId1 = new DefaultIdentifier();
 //        final DefaultIdentifier itemId2 = new DefaultIdentifier();
-        final MemberID memberID = new MemberID("993ffc20-6fd6-11e5-b87f-0a3f44bb4c19");
+        final MemberID memberID = new MemberID();
+        final MerchantID merhantID = new MerchantID();
         System.out.println(memberID.getIdentifier());
-        commandGateway.sendAndWait(new UpdateMemberInfoCommand(memberID, "sunxy2", "12345"));
+//        commandGateway.sendAndWait(new CreatedMemberCommand(memberID, "sunxy2", "12345"));
 //        commandGateway.sendAndWait(new CreateToDoItemCommand(itemId2, "Think about the next steps!"));
+        commandGateway.sendAndWait(new CreateMerchantCommand(merhantID, memberID, "s1", "234", "s2"));
 //        commandGateway.sendAndWait(new MarkCompletedCommand(itemId1));
     }
 }
