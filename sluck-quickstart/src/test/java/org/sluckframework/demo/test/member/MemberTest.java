cbd478bf223b4f390236713ced7d41145cee09cb
sunxueye <317026025@qq.com>
2015-11-21 21:21:25 +0800

    update member test

--- sluck-quickstart/src/test/java/org/sluckframework/demo/test/member/MemberTest.java
+++ sluck-quickstart/src/test/java/org/sluckframework/demo/test/member/MemberTest.java
@@ -2,6 +2,7 @@ package org.sluckframework.demo.test.member;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.sluckframework.common.jdbc.DataSourceConnectionProvider;
 import org.sluckframework.cqrs.commandhandling.annotation.AggregateAnnotationCommandHandler;
 import org.sluckframework.cqrs.commandhandling.disruptor.DisruptorCommandBus;
 import org.sluckframework.cqrs.commandhandling.gateway.CommandGateway;
@@ -9,12 +10,20 @@ import org.sluckframework.cqrs.commandhandling.gateway.DefaultCommandGateway;
 import org.sluckframework.cqrs.eventhanding.EventBus;
 import org.sluckframework.cqrs.eventhanding.SimpleEventBus;
 import org.sluckframework.cqrs.eventsourcing.GenericAggregateFactory;
+import org.sluckframework.cqrs.saga.GenericSagaFactory;
+import org.sluckframework.cqrs.saga.ResourceInjector;
+import org.sluckframework.cqrs.saga.SimpleResourceInjector;
+import org.sluckframework.cqrs.saga.annotation.AnnotatedSagaManager;
+import org.sluckframework.cqrs.saga.repository.jdbc.JdbcSagaRepository;
 import org.sluckframework.demo.member.Member;
+import org.sluckframework.demo.member.Merchant;
+import org.sluckframework.demo.test.member.saga.MerchantCreatedSaga;
 import org.sluckframework.domain.event.eventstore.AggregateEventStore;
 import org.sluckframework.domain.repository.Repository;
 import org.sluckframework.implement.eventstore.jdbc.JdbcEventStore;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 /**
  * Author: sunxy
@@ -22,15 +31,17 @@ import javax.sql.DataSource;
  * Since: 1.0
  */
 public class MemberTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, SQLException {
 
         EventBus eventBus = new SimpleEventBus();
 
         DataSource dataSource = getDataSource();
 
         AggregateEventStore eventStore = new JdbcEventStore(dataSource);
+
         // let's start with the Command Bus
         DisruptorCommandBus commandBus = new DisruptorCommandBus(eventStore, eventBus);
+
 
         // the CommandGateway provides a friendlier API to send commands
         CommandGateway commandGateway = new DefaultCommandGateway(commandBus);
@@ -38,13 +49,44 @@ public class MemberTest {
         // now, we obtain a repository from the command bus
         Repository repository = commandBus.createRepository(new GenericAggregateFactory<>(Member.class));
 
+        Repository repository2 = commandBus.createRepository(new GenericAggregateFactory<>(Merchant.class));
+
         // Axon needs to know that our ToDoItem Aggregate can handle commands
         AggregateAnnotationCommandHandler.subscribe(Member.class, repository, commandBus);
+        AggregateAnnotationCommandHandler.subscribe(Merchant.class, repository2, commandBus);
+
+        // we want to inject resources in our Saga, so we need to tweak the GenericSagaFactory
+        GenericSagaFactory sagaFactory = new GenericSagaFactory();
+        // this will allow the eventScheduler and commandGateway to be injected in our Saga
+        ResourceInjector resourceInjector = new SimpleResourceInjector(commandGateway);
+        sagaFactory.setResourceInjector(resourceInjector);
+
+        // Sagas instances are managed and tracked by a SagaManager.
+//        AnnotatedSagaManager sagaManager = new AnnotatedSagaManager(sagaRepository, sagaFactory, ToDoSaga.class);
+
+        //test with asyn saga
+//        AsyncAnnotatedSagaManager sagaManager = new AsyncAnnotatedSagaManager(MerchantCreatedSaga.class);
+//        sagaManager.setSagaFactory(sagaFactory);
+//        sagaManager.setProcessorCount(2);
+//        sagaManager.start();
+
+        JdbcSagaRepository sagaRepository = new JdbcSagaRepository(new DataSourceConnectionProvider(dataSource));
+        sagaRepository.setResourceInjector(resourceInjector);
+//        sagaRepository.createSchema();
+
+        AnnotatedSagaManager sagaManager = new AnnotatedSagaManager(sagaRepository, sagaFactory, MerchantCreatedSaga.class);
+
+        // and we need to subscribe the Saga Manager to the Event Bus
+        eventBus.subscribe(sagaManager);
 
         // and let's send some Commands on the CommandBus.
         CommandGenerator.sendCommands(commandGateway);
 
+
+        Thread.currentThread().sleep(500000);
+
         commandBus.stop();
+//        sagaManager.stop();
         HikariDataSource ds = (HikariDataSource) dataSource;
         ds.close();
 
@@ -54,13 +96,16 @@ public class MemberTest {
         config.setMaximumPoolSize(10);
         config.setDriverClassName("oracle.jdbc.pool.OracleDataSource");
 //        config.setDataSourceClassName("oracle.jdbc.pool.OracleDataSource");
-        config.setJdbcUrl("jdbc:oracle:thin:@169.254.177.142:1521:XE");
 //        config.addDataSourceProperty("serverName", "169.254.177.142");
+//        config.setJdbcUrl("jdbc:oracle:thin:@169.254.177.142:1521:XE");
+        config.setJdbcUrl("jdbc:oracle:thin:@192.168.1.251:1521:newecpss");
 //        config.addDataSourceProperty("portNumber", "1521");
 //        config.addDataSourceProperty("databaseName", "XE");
 //        config.addDataSourceProperty("driverType", "thin");
-        config.setUsername("sun");
-        config.setPassword("sun");
+//        config.setUsername("sun");
+        config.setUsername("neika");
+//        config.setPassword("sun");
+        config.setPassword("neika");
         config.setConnectionTestQuery("select sysdate from dual");
         return new HikariDataSource(config);
     }
